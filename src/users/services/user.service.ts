47285e7e162e66e5cf15e7611ddfd836e5d66d13
William Mendez <doblealberto@outlook.com>
2023-02-26 23:31:15 -0600

    fix(users): Solved the problem with axios, filter the games that will happen today

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -3,8 +3,10 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as crypto from 'crypto';
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import { User } from '../entities/user.entity';
+import { Games, User } from '../entities/user.entity';
 import { ReceivePredictions } from '../dtos/user.dto';
 import {
   Schedule,
@@ -36,31 +38,44 @@ export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   private readonly API_BASE_URL: string = process.env.API_BASE_URL;
-  private readonly API_KEY: string = process.env.NBA_API_KEY;
+  private readonly API_KEY: string = process.env.API_KEY;
   private readonly lang: string = process.env.LANG;
   private readonly format: string = process.env.FORMAT;
 
-  async getDailySchedule(): Promise<GamesEntity[]> {
-    const date = new Date();
-    // To get the year, the month and the day correctly I had to use the slice method instead of the API of Data object as it was giving me the wrong month.
-    const year = date.toISOString().slice(0, 4);
-    const month = date.toISOString().slice(5, 7);
-    const day = date.toISOString().slice(8, 10);
-    // set a var response with an axios.get call to API_BASE_URL + options to handle authentication of type API_KEY which its API key the format of the call it's the following `${this.API_BASE_ULR}/:locale/games/:year/:month/:day/schedule.${this.format}`
-
+  private async get<T>(endpoint: string): Promise<T> {
+    console.log(
+      `making a request for this endpoint: ${this.API_BASE_URL}/en${endpoint}.${this.format}`
+    );
     const response = await axios.get(
-      `${this.API_BASE_URL}/${this.lang}/games/${year}/${month}/${day}/schedule.${this.format}`,
+      `${this.API_BASE_URL}/en${endpoint}.${this.format}`,
       {
-        params: {
-          api_key: this.API_KEY,
-        },
+        params: { api_key: this.API_KEY },
       }
     );
-    const games: Schedule = response.data;
+    return response.data as T;
+  }
 
-    const upcomingGames = games.games.filter(
-      (game) => new Date(game.scheduled) >= date
+  async getDailySchedule(): Promise<GamesEntity[]> {
+    const date = new Date();
+    const year = date.toISOString().slice(0, 4);
+    const month = date.toISOString().slice(5, 7);
+    const day = date.getDate();
+
+    const games: Schedule = await this.get<Schedule>(
+      `/games/${year}/${month}/${day}/schedule`
     );
+    const upcomingGames = games.games.filter((game) => {
+      const gameDate = new Date(game.scheduled);
+      console.log(gameDate.getFullYear() === date.getFullYear());
+      console.log(gameDate.getMonth() === date.getMonth());
+      console.log(gameDate.getDate() === day, `${gameDate.getDate()}===${day}`);
+      console.log('this is the game date', gameDate);
+      return (
+        gameDate.getFullYear() === date.getFullYear() &&
+        gameDate.getMonth() === date.getMonth() &&
+        gameDate.getDate() === day
+      );
+    });
     return upcomingGames;
   }
 }
