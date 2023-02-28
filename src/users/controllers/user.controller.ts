0b046904f2ea3d3ed34305a75a9c44f034872288
Manuel Parra <th3nolo@gmail.com>
2023-02-28 02:44:05 -0400

    feat: get pred endpoint docu

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { GamesEntity } from '../interfaces/api/api.dailyschedule.interfaces';
 import { PredictionRequest } from '../interfaces/controller/controller.get-prediction.interface';
+import { ApiOperation, ApiTags, ApiBody, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
@@ -13,9 +14,14 @@ export class UserController {
   }
 
   @Post('/get-prediction')
-  async getPrediction(@Body() body: any): Promise<any> {
+  @ApiOperation({
+    summary:
+      'Saves the prediction of the user, needs email: string, address:string, prediction:string',
+  })
+  async getPrediction(@Body() body: PredictionRequest): Promise<any> {
     return this.userService.getPrediction(body);
   }
+
   @Get('/box-scores')
   async getBoxScores(): Promise<any> {
     return this.userService.getBoxScoresForTargetGames();
