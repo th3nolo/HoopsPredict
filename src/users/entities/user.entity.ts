c73a853f2cd67d6f0c8f0245f2d3f8698eb1b355
Manuel Parra <th3nolo@gmail.com>
2023-02-28 02:20:16 -0400

    feat: Save BoxScore and predictions in games.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { Schedule } from '../interfaces/api/api.dailyschedule.interfaces';
 
 @Schema({ collection: 'users' })
 export class User extends Document {
@@ -35,6 +36,7 @@ export class Predictions extends Document {
 export class Games extends Document {
   @Prop({ required: true, unique: true })
   id: string;
+  games: Schedule[];
 }
 
 export const PredictionsSchema = SchemaFactory.createForClass(Predictions);
