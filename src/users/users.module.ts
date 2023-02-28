4123011771e5d5abc891b1f489778a41ef328988
William Mendez <doblealberto@outlook.com>
2023-02-27 23:47:22 -0600

    feat(users.service): Add the configurations for the new model

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Games, GamesSchema, User, UserSchema } from './entities/user.entity';
+import {
+  Games,
+  GamesSchema,
+  Predictions,
+  PredictionsSchema,
+  User,
+  UserSchema,
+} from './entities/user.entity';
 import { ScheduleModule } from '@nestjs/schedule';
 import { StoreGamesCron } from './cronjobs/store.games';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Games.name, schema: GamesSchema }]),
+    MongooseModule.forFeature([
+      { name: Predictions.name, schema: PredictionsSchema },
+    ]),
     ScheduleModule.forRoot(),
   ],
   providers: [UserService, StoreGamesCron],
